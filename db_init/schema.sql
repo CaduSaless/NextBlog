acca326afbd60546c2729ca2d04dec3e90d7b6e4
CaduSaless <cadubss2@gmail.com>
2025-11-05 15:01:39 -0300

    Docker-compose e init_db

---
+++ db_init/schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS Usuarios (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    senha_hash VARCHAR(255) NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS Posts (
+    id SERIAL PRIMARY KEY,
+    resumo VARCHAR(300) NOT NULL,
+    corpo TEXT NOT NULL,
+    titulo VARCHAR(200) NOT NULL,
+    add_imagem VARCHAR(255) NOT NULL,
+    autor_id INT REFERENCES Usuarios(id),
+);
